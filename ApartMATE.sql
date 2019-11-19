f452ce48cd38cb194548a365dc142a24eceefc50
Andrew Smith <smitha24@rpi.edu>
2019-11-19 18:22:49 -0500

    Updated Database File

--- ApartMATE.sql
+++ ApartMATE.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 12, 2019 at 07:55 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
+-- Generation Time: Nov 19, 2019 at 11:21 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `ApartMATE`
 --
-CREATE DATABASE IF NOT EXISTS `ApartMATE` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `ApartMATE` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `ApartMATE`;
 
 -- --------------------------------------------------------
@@ -32,18 +32,19 @@ USE `ApartMATE`;
 
 CREATE TABLE `listings` (
   `listing_id` int(100) NOT NULL,
-  `address` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `price` int(100) NOT NULL,
   `sqft` int(50) NOT NULL,
   `bed` int(2) NOT NULL,
   `bath` int(2) NOT NULL,
-  `description` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   `times` int(255) NOT NULL,
   `rating` int(2) NOT NULL,
-  `type` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `utilites` set('yes','partial','no') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `landlord` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `type` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `utilites` int(1) NOT NULL,
+  `landlord` smallint(3) NOT NULL,
+  `Picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -52,14 +53,14 @@ CREATE TABLE `listings` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `full_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` smallint(3) NOT NULL,
+  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `full_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `phone_number` int(15) NOT NULL,
-  `rating` int(2) NOT NULL,
-  `user_type` set('landlord','renter') COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `rating` int(2) DEFAULT NULL,
+  `user_type` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Indexes for dumped tables
@@ -69,7 +70,8 @@ CREATE TABLE `users` (
 -- Indexes for table `listings`
 --
 ALTER TABLE `listings`
-  ADD PRIMARY KEY (`listing_id`);
+  ADD PRIMARY KEY (`listing_id`),
+  ADD KEY `landlord` (`landlord`);
 
 --
 -- Indexes for table `users`
@@ -91,9 +93,22 @@ ALTER TABLE `listings`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` smallint(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `listings`
+--
+ALTER TABLE `listings`
+  ADD CONSTRAINT `listings_ibfk_1` FOREIGN KEY (`landlord`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'test'@'%' IDENTIFIED BY PASSWORD '*C44E1F4F0C910DD1FDB5DCE5CA243964A07E2EDF';
+
