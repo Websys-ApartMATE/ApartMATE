81181ec0bdb2cdd21d3019eb7de934af7c9eefd0
Winston Yu <yuy9@rpi.edu>
2019-12-02 21:43:05 -0500

    commit Dynamic ListingPage with Data

---
+++ listingPage/ApartMATE.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 03, 2019 at 02:15 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.4.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ApartMATE`
+--
+CREATE DATABASE IF NOT EXISTS `ApartMATE` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `ApartMATE`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `msg_to` smallint(3) NOT NULL,
+  `msg_from` smallint(3) NOT NULL,
+  `contents` varchar(1000) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`timestamp`, `msg_to`, `msg_from`, `contents`) VALUES
+('2019-12-03 01:35:02', 2, 1, 'Hello World'),
+('2019-12-03 01:35:26', 1, 2, 'World Hello'),
+('2019-12-03 01:36:25', 3, 1, 'Test Test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listings`
+--
+
+CREATE TABLE `listings` (
+  `listing_id` int(100) NOT NULL,
+  `zip` int(11) NOT NULL,
+  `street` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `city` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `state` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(100) NOT NULL,
+  `sqft` int(50) NOT NULL,
+  `bed` int(2) NOT NULL,
+  `bath` int(2) NOT NULL,
+  `description` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `times` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `rating` int(2) NOT NULL,
+  `Picture` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `landlord` smallint(3) NOT NULL,
+  `name` varchar(256) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `listings`
+--
+
+INSERT INTO `listings` (`listing_id`, `zip`, `street`, `city`, `state`, `price`, `sqft`, `bed`, `bath`, `description`, `times`, `rating`, `Picture`, `landlord`, `name`) VALUES
+(1, 12180, '110 Colleen Rd', 'Troy', 'New York', 1783, 1200, 2, 1, 'This is a great place to rent. The internet is really fast, theres heating, ceiling fans, and its smoke free. Looking for student tenants for the Spring Semester ', 'Mondays 5pm - 9pm', 3, '../resources/countryGardenPic.jpg', 2, 'Country Garden Apartments');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` smallint(3) NOT NULL,
+  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `full_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `phone_number` int(15) NOT NULL,
+  `dob` date NOT NULL,
+  `rating` int(2) DEFAULT NULL,
+  `user_type` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `password`, `full_name`, `phone_number`, `dob`, `rating`, `user_type`) VALUES
+(1, 'test@rpi.edu', 'test', 'Andrew Smith', 111111, '2019-12-02', NULL, NULL),
+(2, 'test2@rpi.edu', 'test2', 'Bandrew Smith', 1111112, '2019-12-18', NULL, NULL),
+(3, 'test3@rpi.edu', 'test3', 'Candrew Smith', 1111113, '2019-12-11', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`timestamp`),
+  ADD KEY `msg_from` (`msg_from`),
+  ADD KEY `msg_to` (`msg_to`);
+
+--
+-- Indexes for table `listings`
+--
+ALTER TABLE `listings`
+  ADD PRIMARY KEY (`listing_id`),
+  ADD KEY `zip` (`zip`),
+  ADD KEY `landlord` (`landlord`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `listings`
+--
+ALTER TABLE `listings`
+  MODIFY `listing_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` smallint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chat`
+--
+ALTER TABLE `chat`
+  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`msg_from`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`msg_to`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `listings`
+--
+ALTER TABLE `listings`
+  ADD CONSTRAINT `listings_ibfk_1` FOREIGN KEY (`landlord`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
