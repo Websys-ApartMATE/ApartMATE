1213764b95057f19d90ac26edd824be4101cf3e5
molly34672 <meixinliang8@gmail.com>
2019-12-03 08:48:24 -0500

    fixed chatPage navbar by including font awesome icon import statement

--- chatPage/chatPage.php
+++ chatPage/chatPage.php
@@ -66,28 +66,12 @@ if ($data['msg_to'] == $user_id) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!--Font awesome icons-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
 </head>
 <body>
-<!--Navigation Bar--------------------------------------------------------------------------------------->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><img id="piclogo" src="logo.png" alt="ApartMATE logo"/></a>
-    <button class="navbar-light navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse navbar-light" id="collapsibleNavbar">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Rent</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Add a Listing</a>
-            </li>
-        </ul>
-        <button type="button" class="btn btn-outline-secondary"
-                onclick="window.location.href='../homePage/homePage.html'"
-        ">Sign Out</button>
-    </div>
-</nav>
+<!--Navigation Bar--------------------------------------------->
+<?php include('../navBar/navBar.php')?>
 
 <!--Message Box--------------------------------------------------------------------------------------->
 <div class="container">
